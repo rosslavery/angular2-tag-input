d24254d417b50a7d7a0ec9f1971416d474333a7a
codiak <592344+codiak@users.noreply.github.com>
2018-10-23 11:52:59 -0500

    v2.0.0 beta: Version updates

--- demo/main.ts
+++ demo/main.ts
@@ -3,5 +3,6 @@ import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
 
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
